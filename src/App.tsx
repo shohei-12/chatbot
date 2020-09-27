237662b7a14429beae8c13cbd947327209eaa2ad
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-09-27 19:31:06 +0900

    Create root component (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import defaultDataset from "./dataset.js";
+import "./assets/styles/style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppState {
+  answers: string[];
+  chats: string[];
+  currentId: string;
+  dataset: object;
+  open: boolean;
 }
 
-export default App;
+export default class App extends React.Component<{}, AppState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentId: "init",
+      dataset: defaultDataset,
+      open: false,
+    };
+  }
+  render() {
+    return (
+      <section className="c-section">
+        <div className="c-box"></div>
+      </section>
+    );
+  }
+}
